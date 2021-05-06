ed6e642f9d73865f6958f25aac9c3ef378f84f5e
dungki <67158859+dungki@users.noreply.github.com>
2021-05-06 18:04:59 +0700

    firts update

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="profile-card">
+    <div class="card-header">
+      <div class="pic">
+        <img src="image/avatar.jpg" alt="">
+      </div>
+      <div class="name">Nguyễn Hồng huy</div>
+      <div class="sm">
+        <a href="hhttps://www.facebook.com/huy.27022001/" class="fab fa-facebook-f" target="_blank"></a>
+        <a href="https://www.instagram.com/huy.201/?fbclid=IwAR09R9F39RMbJ_KWQjDUijb-V3Ewp5WGKDNirV22REVZ0JPdK94_JUifNf4" class="fab fa-instagram" target="_blank"></a>
+        <a href="mailto: nguyenhonghuypt@gmail.com" class="far fa-envelope" target="_blank"></a>
+        <a href="http://zaloapp.com/qr/p/i32a8ojdr56d" class="zalo" target="_blank">ZaLo</a>
+      </div>
+      <a href="tel:0921812702" class="contact-btn">Contact Me</a>
+    </div>
+  </div>
+</body>
+</html>
